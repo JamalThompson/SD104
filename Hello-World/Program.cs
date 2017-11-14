26f809976b1499d0a174779c6240abaa9536c8d4
Jamal <jthompson6799@gmail.com>
2017-11-13 19:50:18 -0600

    Add project files.

---
+++ Hello-World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
